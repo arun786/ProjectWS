f47ac331f40a2136b22b0da64ae406d31c73708a
arun789 <adwiti1975@gmail.com>
2017-02-26 02:02:07 -0700

    load data from db and laod it to cache

--- GenericRestService/src/main/java/com/arun/util/CacheUtil.java
+++ GenericRestService/src/main/java/com/arun/util/CacheUtil.java
@@ -1,6 +1,8 @@
 package com.arun.util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,10 +15,19 @@ public class CacheUtil {
 	@Autowired
 	private ConfigurationDetails configurationDetails;
 
+	private static Map<String, Consumer> consumerDetails = new HashMap<>();
+
 	@Scheduled(cron = "*/10 * * * * *")
 	public void cache() {
 		List<ConfigurationBean> lstConfig = configurationDetails.fetchConfigurationDetails();
-		System.out.println(lstConfig);
-	}
 
+		for (ConfigurationBean beans : lstConfig) {
+			Consumer consumer = new Consumer();
+			consumer.setConsumerId(beans.getConsumerId());
+			consumer.setConsumerName(beans.getName());
+			consumer.setAttributes(beans.getAttributes());
+
+			consumerDetails.put(beans.getConsumerId(), consumer);
+		}
+	}
 }
---
+++ GenericRestService/src/main/java/com/arun/util/Consumer.java
@@ -0,0 +1,33 @@
+package com.arun.util;
+
+public class Consumer {
+
+	private String consumerId;
+	private String consumerName;
+	private String attributes;
+
+	public String getConsumerId() {
+		return consumerId;
+	}
+
+	public void setConsumerId(String consumerId) {
+		this.consumerId = consumerId;
+	}
+
+	public String getConsumerName() {
+		return consumerName;
+	}
+
+	public void setConsumerName(String consumerName) {
+		this.consumerName = consumerName;
+	}
+
+	public String getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(String attributes) {
+		this.attributes = attributes;
+	}
+
+}
